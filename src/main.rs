c8fec190912b898b4b9a11609b5c9ba1ae2557a1
Sylvan Smit <sylvan@sylvansmit.com>
2025-08-18 21:15:44 +0200

    Write to output file
    Signed-off-by: Sylvan Smit <sylvan@sylvansmit.com>

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
-// Plan 1: Read in a RSS xml file and print the data to console or generate an html file to read
+// Plan 1: Read in a RSS xml file and generate an html file to read
 use rss::Channel;
+use rss::validation::Validate;
 use std::fs::File;
 use std::io::BufReader;
 
 fn main() {
     let file = File::open("feed.xml").unwrap();
     let channel = Channel::read_from(BufReader::new(file)).unwrap();
-    println!("{channel}");
+
+    channel.validate().expect("Invalid RSS file.");
+
+    let output_file = File::create("output.xml").unwrap();
+    channel
+        .write_to(output_file)
+        .expect("Failed to write to output file.");
+    println!("Wrote to output file")
 }
