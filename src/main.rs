8c3df8c36af00e42b18a654b0337d1a4742f77a7
Sylvan Smit <sylvan@sylvansmit.com>
2025-08-18 21:03:38 +0200

    Read in feed.xml and print to console
    Signed-off-by: Sylvan Smit <sylvan@sylvansmit.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+// Plan 1: Read in a RSS xml file and print the data to console or generate an html file to read
+use rss::Channel;
+use std::fs::File;
+use std::io::BufReader;
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("feed.xml").unwrap();
+    let channel = Channel::read_from(BufReader::new(file)).unwrap();
+    println!("{channel}");
 }
