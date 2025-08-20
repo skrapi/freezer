acfa1e8aef57a82c1b2a1d5f3ab40c86e2b23852
Sylvan Smit <sylvan@sylvansmit.com>
2025-08-20 18:00:10 +0200

    Send email when publish is called
    Signed-off-by: Sylvan Smit <sylvan@sylvansmit.com>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::{Parser, Subcommand};
-use freezer::subscriber::Subscriber;
+use freezer::configuration::Configuration;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -39,12 +39,68 @@ enum Commands {
     },
 }
 
-fn main() {
-    let subscriber = Subscriber::from_config_file("/home/skrapi/.config/freezer/freezer.toml");
+use home::home_dir;
+use lettre::message::header::ContentType;
+use lettre::transport::smtp::authentication::Credentials;
+use lettre::{Message, SmtpTransport, Transport};
 
-    println!("{subscriber:?}");
+pub async fn send_wrap_up_email(
+    to_name: String,
+    to_email: String,
+    body: String,
+    cred_email: String,
+    cred_password: String,
+) -> Result<(), Box<dyn std::error::Error>> {
+    // Create email message
+    let email = Message::builder()
+        .from("Freezer <freezer@sylvansmit.com>".parse()?)
+        .to(format!("{to_name} <{to_email}>").parse()?)
+        .subject("Test email from Rust!")
+        .header(ContentType::TEXT_PLAIN)
+        .body(body)?;
+
+    let creds = Credentials::new(cred_email, cred_password);
+
+    // Create SMTP transport
+    let mailer = SmtpTransport::relay("smtp.fastmail.com")?
+        .credentials(creds)
+        .build();
+
+    // Send the email
+    match mailer.send(&email) {
+        Ok(_) => println!("Email sent successfully!"),
+        Err(e) => println!("Could not send email: {e:?}"),
+    }
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let config = if let Some(path) = home_dir()
+        && path.exists()
+        && path.join(".config/freezer/freezer.toml").exists()
+    {
+        Configuration::from_config_file(path.join(".config/freezer/freezer.toml"))
+    } else {
+        println!("You appear to be missing your config file");
+        return Ok(());
+    };
 
     let cli = Cli::parse();
-
-    println!("{cli:?}");
+    match cli.command {
+        Commands::Add { email, feed } => todo!(),
+        Commands::Remove { email, feed } => todo!(),
+        Commands::Publish { email } => {
+            send_wrap_up_email(
+                "Sylvan".to_owned(),
+                email.unwrap(),
+                "Test publish command".to_owned(),
+                config.sender.app_email,
+                config.sender.app_password,
+            )
+            .await?
+        }
+    };
+    Ok(())
 }
